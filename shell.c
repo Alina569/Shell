9f91bfb4bea7e64b49e3bc8baead4020ed571c6c
Alina <alipercu@gmail.com>
2017-10-15 14:06:18 -0400

    Added basic control flow

--- shell.c
+++ shell.c
@@ -1,5 +1,23 @@
-int main() {
-
-	return 0;
-
+#include "shell.h"
+int main(int argc, char **argv){
+    
+    char *line = (char*) malloc(sizeof(char)*INPUT_SIZE);
+    
+    // do something with the history file
+    int status = TRUE;
+    while(status){
+        
+        printf(">>");
+        fflush(stdout);
+        parse_input(line);
+        
+        if (strlen(line) != 0) {
+            // do something
+        }
+    }
+    // close the history file
+    return 0;
+}
+int parse_input(char *line){
+    return 0;
 }
