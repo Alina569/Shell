797f44a27a893af7e33b2541ae527b2b352136b6
Alina <alipercu@gmail.com>
2017-11-11 13:36:02 -0500

    Basic working shell, only execvp

--- shell.c
+++ shell.c
@@ -7,28 +7,32 @@ int main(int argc, char **argv){
 
 	struct Process *commands;
 
+	configure();
+
 	do {
 		printf("> ");
 		fflush(stdout);
 
 		parse_input(line);
 		fflush(stdout);
-
-		commands = parse_commands(line);
-
+		if(strlen(line) != 0) {
+			commands = parse_commands(line);
+			status = execute_command(commands);
+		}
 	} while(status);
 
+	recover_state();
 	return 0;
 }
 
 
 int parse_input(char *line) {
-	
+
 	char buffer;
 	int position = 0;
-	
+
 	// int history_nav = history_count;
-	
+
 	while(read(0, &buffer, 1) >= 0) {
 		if (buffer > 0) {
 			switch (buffer) {
@@ -36,28 +40,28 @@ int parse_input(char *line) {
 					read(0, &buffer, 1);
 					read(0, &buffer, 1);
 
-					printf("buffer");
-					break;
+					printf("Arrow key");
+				break;
 				case BACKSPACE:
 					if (position >= 1) {
 						write(2, "\10\33[1P", 5);
 						position--;
 					}
-					break;
+				break;
 				case DELETE:
 					write(2, "\33[1P", 4);
-					break;
+				break;
 				case ENTER:
 					line[position] = '\0';
 					printf("\n");
 					fflush(stdout);
 					return 1;
-					break;
+				break;
 				default:
 					line[position] = buffer;
 					position++;
 					write(2, &buffer, 1);
-					break;
+				break;
 			}
 		}
 	}
@@ -73,7 +77,7 @@ struct Process *parse_commands(char* line) {
 
 	process->argc = 0;
 	process->argv = (char**) malloc(INPUT_SIZE * sizeof(char));
-	
+
 	// global config
 	background_flag = FALSE;
 	fileOut = NULL;
@@ -81,19 +85,31 @@ struct Process *parse_commands(char* line) {
 
 	// local config
 	str_init = 0;
+	position = 0;
 
 	token = strtok(line, " ");
 	while(token != NULL){
 		switch(token[0]){
-
+			// add | > < & and quotes
+			case '&':
+				background_flag = TRUE;
+			break;
 			default:
 				process->argc++;
 				process->argv[position] = token;
+
 				if (str_init) {
 					token = strtok(NULL, " ");
 					while(token != NULL){
-						printf("TOKEN TOKEN");
+						process->argv[position] = concat(process->argv[position], token);
+
+						if (token[strlen(token)-1] == '\"' || token[strlen(token)-1] == '\'') {
+							break;
+						}
+						token = strtok(NULL, " ");
 					}
+					process->argv[position]++;
+					process->argv[position][strlen(process->argv[position])-1] = '\0';
 				}
 				position++;
 			break;
@@ -104,7 +120,16 @@ struct Process *parse_commands(char* line) {
 	return process;
 }
 
+int execute_command(struct Process *process) {
+	pid_t pid;
 
+	int status;
 
-
+	pid = fork();
+	if (pid == 0){
+		status = execvp(process->argv[0], process->argv);
+		return status;
+	}
+	return -1;
+}
 
